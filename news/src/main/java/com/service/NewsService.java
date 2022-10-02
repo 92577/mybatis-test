dcacd52a7da582a7b7bc2e4428b1e9a83ceb2e8d
lfc <2413721296@qq.com>
2022-10-02 08:35:00 +0800

    测试git提交

--- news/src/main/java/com/service/NewsService.java
+++ news/src/main/java/com/service/NewsService.java
@@ -2,11 +2,13 @@ package com.service;
 
 import com.domain.News;
 import com.mapper.NewsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NewsService
 {
+	@Autowired
 	private NewsMapper newsMapper;
 
 	public News selectNews(int id)
