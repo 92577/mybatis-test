2884d30189ca089030b218b1436164f916823b20
lfc <2413721296@qq.com>
2022-10-02 08:31:59 +0800

    测试git提交

--- news/src/main/java/com/mapper/NewsMapper.java
+++ news/src/main/java/com/mapper/NewsMapper.java
@@ -2,11 +2,12 @@ package com.mapper;
 
 import com.domain.News;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface NewsMapper
 {
 	@Select("select * from news where id = #{id}")
-	News select(int id);
+	News select(@Param("id") int id);
 }
