4b61953feec7134770aec513df668a8357a8576b
lfc <2413721296@qq.com>
2022-10-02 08:10:37 +0800

    测试提交，完成mapper接口

--- news/src/main/java/mapper/NewsMapper.java
+++ news/src/main/java/mapper/NewsMapper.java
@@ -1,5 +1,12 @@
 package mapper;
 
+import com.News;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface NewsMapper
 {
+	@Select("select * from news where id = #{id}")
+	News select(int id);
 }
