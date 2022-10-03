85da01ce8c74e9ceb6539d21e420f526f10b6475
lfc <2413721296@qq.com>
2022-10-03 08:12:34 +0800

    测试提交，配置跨域

--- news/src/main/java/com/config/CrosConfig.java
+++ news/src/main/java/com/config/CrosConfig.java
@@ -1,7 +1,6 @@
 package com.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,11 +10,15 @@ public class CrosConfig implements WebMvcConfigurer
 	@Override
 	public void addCorsMappings(CorsRegistry registry)
 	{
-		registry.addMapping("/**").
-				allowedOrigins("*"). //允许跨域的域名，可以用*表示允许任何域名使用
-				allowedMethods("*"). //允许任何方法（post、get等）
-				allowedHeaders("*"). //允许任何请求头
-				allowCredentials(true). //带上cookie信息
-				exposedHeaders(HttpHeaders.SET_COOKIE).maxAge(3600L); //maxAge(3600)表明在3600秒内，不需要再发送预检验请求，可以缓存该结果
+		registry.addMapping("/**")
+				// 设置允许跨域请求的域名
+				.allowedOriginPatterns("*")
+				// 是否允许证书（cookies）
+				.allowCredentials(true)
+				// 设置允许的方法
+				.allowedMethods("*")
+				.allowedHeaders("*")
+				// 跨域允许时间
+				.maxAge(3600);
 	}
 }
